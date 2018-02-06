15d3a208c10be021ff00c24080ea0256ce510667
HacKan <hackan@gmail.com>
2018-02-05 23:47:46 -0300

    Bump to v1.0.0rc1
    Prepare for PyPi.

--- setup.py
+++ setup.py
@@ -8,39 +8,42 @@ def readme():
         return rst.read()
 
 
-setup(name='passphrase',
-      version=passphrase_version,
-      description='Generates cryptographically secure passphrases and '
-                  'passwords',
-      long_description=readme(),
-      classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: GNU General Public License v3 or later '
-        '(GPLv3+)',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.2',
-        'Topic :: Security :: Cryptography',
-        'Topic :: Utilities'
-      ],
-      platforms=[
-        'POSIX :: Linux'
-      ],
-      keywords='cryptography passphrase password security',
-      url='http://github.com/hackancuba/passphrase-py',
-      author='HacKan',
-      author_email='hackan@gmail.com',
-      license='GNU GPL 3.0+',
-      packages=['passphrase'],
-      python_requires='~=3.2',
-      install_requires=[
-      ],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      entry_points={
-          'console_scripts': ['passphrase=passphrase.__main__:main'],
-      },
-      zip_safe=False)
+setup(
+    name='passphrase',
+    version=passphrase_version,
+    description='Generates cryptographically secure passphrases and '
+                'passwords',
+    long_description=readme(),
+    classifiers=[
+      'Development Status :: 5 - Production/Stable',
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'Intended Audience :: End Users/Desktop',
+      'License :: OSI Approved :: GNU General Public License v3 or later '
+      '(GPLv3+)',
+      'Natural Language :: English',
+      'Operating System :: POSIX :: Linux',
+      'Programming Language :: Python :: 3.2',
+      'Topic :: Security :: Cryptography',
+      'Topic :: Utilities'
+    ],
+    platforms=[
+      'POSIX :: Linux'
+    ],
+    keywords='cryptography passphrase password security',
+    url='http://github.com/hackancuba/passphrase-py',
+    download_url='https://github.com/HacKanCuBa/passphrase-py/archive/v1.0.0rc1.tar.gz',
+    author='HacKan',
+    author_email='hackan@gmail.com',
+    license='GNU GPL 3.0+',
+    packages=['passphrase'],
+    python_requires='~=3.2',
+    install_requires=[
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    entry_points={
+        'console_scripts': ['passphrase=passphrase.__main__:main'],
+    },
+    zip_safe=False
+)
